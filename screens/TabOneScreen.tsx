c2018f9b4b4b9089eda4976f23442a229c1619fb
Jefferson Bastos <jeff.junior98@gmail.com>
2020-12-05 20:07:29 -0300

    start project

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -7,7 +7,7 @@ import { Text, View } from '../components/Themed';
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Termometro Cerveja </Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.js" />
     </View>
@@ -19,8 +19,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor:"#7B68EE"
   },
   title: {
+    color: "#FF6347",
     fontSize: 20,
     fontWeight: 'bold',
   },
